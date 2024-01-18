a6e732cfec2551a39456358ee90534e08dd7fdbe
Manik Mistry <mkmistryhd2019@gmail.com>
2024-01-18 12:54:41 +0530

    Interview Prep

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ let bool=true;
 let undef=undefined;
 let nullVar=null;
 let sym=Symbol('unique')
+
 // Non-Primitive Datatype
+const arr=[1,2,3,"hello",null,undefined,true,false];
+const obj={a:3, b:2};
+
+// let var const
