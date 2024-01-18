f90bedbf99d2c7388f6610fee6c8db2e2406fcd0
Manik Mistry <mkmistryhd2019@gmail.com>
2024-01-18 12:52:55 +0530

    Interview Prep

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+// Data Types
+// Primitive Datatype
+let num=10;
+let str='hello';
+let bool=true;
+let undef=undefined;
+let nullVar=null;
+let sym=Symbol('unique')
+// Non-Primitive Datatype
